3237c573296c4d339ab7e52a8fc9ea3834265305
Michael Salvati <print@3daddict.com>
2018-10-26 14:22:39 -0700

    Delete email_config.php

--- php_mailer/email_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('EMAIL_USER','mikedoesdevserver@gmail.com');
-define('EMAIL_USERNAME', 'Portfolio ContactForm');
-define('EMAIL_PASS','76aSy32Hn');
-define('EMAIL_TO_ADDRESS','info@msalvati.com');
-?>
