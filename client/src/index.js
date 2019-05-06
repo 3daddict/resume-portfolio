05503b6a8f2bb0d7ca2d356509cef96254c57691
Michael <print@3daddict.com>
2019-05-05 22:05:52 -0700

    webpack configured with babel, sass, dev and html

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import './main.scss';
+
+const injectText = () => {
+    
+ const container = document.getElementById('root');
+ container.innerText = "This code is injected with Javascript!!!"
+
+}
+
+injectText();
